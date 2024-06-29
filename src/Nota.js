ef7e6f318042ac8544147dfb956cfed7807c3cfa
Alexander Robles <alexanderrobles12@outlook.com>
2024-06-28 20:58:17 -0600

    Initial commit

--- src/Nota.js
+++ src/Nota.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './Nota.css'; // Archivo CSS para los estilos específicos de las notas
+import './Nota.css';
 
 const Nota = ({ titulo, texto, imagen }) => {
   return (
-    <div className="nota blur-background">
+    <div className="nota">
       <h3>{titulo}</h3>
       <p>{texto}</p>
-      {imagen && <img src={imagen} alt="Nota Imagen" className="nota-imagen" />}
+      {imagen && <img src={imagen} alt="Nota" />}
     </div>
   );
 };
--- src/NoteBoard.js
+++ src/NoteBoard.js
@@ -6,7 +6,7 @@ const NoteBoard = ({ notas }) => {
   return (
     <div className="note-board">
       {notas.map((nota) => (
-        <Nota key={nota.id} titulo={nota.titulo} texto={nota.texto} />
+        <Nota key={nota.id} titulo={nota.titulo} texto={nota.texto} imagen={nota.imagen} />
       ))}
     </div>
   );
