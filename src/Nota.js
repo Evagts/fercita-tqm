27bac1dc9a7b8305b0c03fc4c01d209f92248ed4
Alexander Robles <alexanderrobles12@outlook.com>
2024-06-28 20:32:22 -0600

    Initial commit

--- src/Nota.js
+++ src/Nota.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Nota.css'; // Archivo CSS para los estilos específicos de las notas
 
-const Nota = ({ titulo, texto }) => {
+const Nota = ({ titulo, texto, imagen }) => {
   return (
-    <div className="nota">
+    <div className="nota blur-background">
       <h3>{titulo}</h3>
       <p>{texto}</p>
+      {imagen && <img src={imagen} alt="Nota Imagen" className="nota-imagen" />}
     </div>
   );
 };
